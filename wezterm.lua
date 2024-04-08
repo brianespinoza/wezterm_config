4bb5dd1eb2c2fa6291a1b5c2266a05f2ce495913
Brian Espinoza <briane@uci.edu>
2024-04-08 15:23:35 -0700

    config update

--- wezterm.lua
+++ wezterm.lua
@@ -1,60 +1,58 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local w = require('wezterm')
 
--- smartsplits.nvim
-local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+    -- this is set by the plugin, and unset on ExitPre in Neovim
+    return pane:get_user_vars().IS_NVIM == 'true'
+end
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Kanagawa (Gogh)'
--- config.font = wezterm.font 'JetBrains Mono'
-config.font = wezterm.font 'Fira Code'
-config.default_cwd = os.getenv("HOME") .. "/Source";
-
--- you can put the rest of your Wezterm config here
-smart_splits.apply_to_config(config, {
-  -- the default config is here, if you'd like to use the default keys,
-  -- you can omit this configuration table parameter and just use
-  -- smart_splits.apply_to_config(config)
-
-  -- directional keys to use in order of: left, down, up, right
-  direction_keys = { 'h', 'j', 'k', 'l' },
-  -- modifier keys to combine with direction_keys
-  modifiers = {
-    move = 'META', -- modifier to use for pane movement, e.g. CTRL+h to move left
-    resize = 'CTRL', -- modifier to use for pane resize, e.g. META+h to resize to the left
-  },
-})
-
-config.keys = {
-  {
-    key = '!',
-    mods = 'META',
-    action = wezterm.action_callback(function(win, pane)
-      local tab, window = pane:move_to_new_tab()
-    end),
-  },
-}
-config.hyperlink_rules = {
-	-- linkify email addresses
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{ regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]], format = "mailto:$0" },
-	-- file:// URI
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{ regex = [[\bfile://\S*\b]], format = "$0" },
-
-	-- Make username/project paths clickable. This implies paths like the following are for GitHub.
-	-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-	{ regex = [["([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)"]], format = "https://www.github.com/$1/$3" },
-
-	-- { regex = [[localhost:(\d+)]], format = "http://localhost:$1" },
+local direction_keys = {
+    h = 'Left',
+    j = 'Down',
+    k = 'Up',
+    l = 'Right',
 }
 
--- and finally, return the configuration to wezterm
-return config
+local function split_nav(resize_or_move, key)
+    return {
+        key = key,
+        mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+        action = w.action_callback(function(win, pane)
+            if is_vim(pane) then
+                -- pass the keys through to vim/nvim
+                win:perform_action({
+                    SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+                }, pane)
+            else
+                if resize_or_move == 'resize' then
+                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+                else
+                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+                end
+            end
+        end),
+    }
+end
+
+
+local defaultfont = w.font 'Fira Code'
+local default_cwd = os.getenv("HOME") .. "/Source"
+
+return {
+    default_cwd = default_cwd,
+    font = defaultfont,
+    color_scheme = 'Kanagawa (Gogh)',
+    keys = {
+        -- move between split panes
+        split_nav('move', 'h'),
+        split_nav('move', 'j'),
+        split_nav('move', 'k'),
+        split_nav('move', 'l'),
+        -- resize panes
+        split_nav('resize', 'h'),
+        split_nav('resize', 'j'),
+        split_nav('resize', 'k'),
+        split_nav('resize', 'l'),
+    },
+}
